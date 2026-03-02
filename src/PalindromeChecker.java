8b163fc498017ba8c5a11ab4c43fe6bfca5cb7f6
Aryabhatt04 <dhruvkaushik2004@gmail.com>
2026-03-02 22:43:36 +0530

    Base application setup

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,8 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to PalindromeChecker App");
+        System.out.println("Application Name: PalindromeChecker");
+        System.out.println("Application Version: 1.0");
+    }
+}
